f38c58adc8b2e963378794d4a82e7a57364c3f81
TechForEverybody <Pankajsinghat95@gmail.com>
2023-03-07 12:04:45 +0530

    CORS Handled

--- index.py
+++ index.py
@@ -24,7 +24,7 @@ def not_found(error):
 
 if __name__ == '__main__':
     # try:
-        app.run(debug=True,host='0.0.0.0', port=80)
+        app.run(debug=True,host='0.0.0.0', port=8080)
         mongodb_connection.close()
     # except:
     #     pass
