f10d5dc37df10ed5442e8c46db28f38e9395e4dd
TechForEverybody <Pankajsinghat95@gmail.com>
2023-03-07 08:48:49 +0530

    CORS Error Handled

--- App.py
+++ App.py
@@ -4,10 +4,12 @@ import hashlib
 import datetime
 import bson.json_util
 from Modules.validate_id import *
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.secret_key="SHIVKUMAR_CHAUHAN"
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 def isPostMethod():
     if request.method=="POST":return True
