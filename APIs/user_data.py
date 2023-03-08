5948495e26053040dbb53f81a3028c34a19e7bf5
TechForEverybody <Pankajsinghat95@gmail.com>
2023-03-08 08:48:33 +0530

    Irrigation Value is Done

--- APIs/user_data.py
+++ APIs/user_data.py
@@ -14,7 +14,7 @@ def getDashboardData():
             humidity_data=[]
             moisture_data=[]
             for module_id in iot_modules_list:
-                data=sensors_data.find({"iot_module_id":module_id},{"temperature":1,"humidity":1,"soil_moisture":1}).limit(10)
+                data=sensors_data.find({"iot_module_id":module_id},{"temperature":1,"humidity":1,"soil_moisture":1}).limit(25).sort("_id",-1)
                 data=[i for i in data]
                 print(data)
                 temperature_data.append([i['temperature'] for i in data])
@@ -45,16 +45,23 @@ def getIrrigationData():
     if isPostMethod():
         if isLoggedIn():
             iot_module_id=request.json["module_id"]
-            irrigation_data=sensors_data.find({"iot_module_id":iot_module_id},{"temperature":1,"humidity":1,"soil_moisture":1,"_id":0,"timestamp":1}).limit(10)
+            irrigation_data=sensors_data.find({"iot_module_id":iot_module_id,"owner_id":session['id']},{"temperature":1,"humidity":1,"soil_moisture":1,"_id":0,"timestamp":1}).limit(25).sort("_id",-1)
             irrigation_data=[i for i in irrigation_data]
             module_data=iot_modules.find({"_id":bson.objectid.ObjectId(iot_module_id)},{"_id":0,"owner_id":0,"sensor_key":0})
             module_data=[i for i in module_data]
             temperature_data=[i['temperature'] for i in irrigation_data]
+            temperature_data.reverse()
             humidity_data=[i['humidity'] for i in irrigation_data]
+            humidity_data.reverse()
             soil_moisture_data=[i['soil_moisture'] for i in irrigation_data]
+            soil_moisture_data.reverse()
             time_stamp=[i['timestamp'] for i in irrigation_data]
+            time_stamp.reverse()
+            data=sensors_data.find({"iot_module_id":iot_module_id},{"_id":0,"iot_module_id":0}).limit(1).sort("_id",-1)
+            data=[i for i in data]
             final_data={
                 "module_data":module_data,
+                "recent_data":data[0] if len(data)>0 else "No data",
                 "irrigation_data":{
                     "temperature_data":temperature_data,
                     "humidity_data":humidity_data,
